b24aa588410261b7f1006b384a3437b3975e3ee7
mustaphahaadi <mustaphahaadi04@gmail.com>
2025-05-09 22:15:20 +0000

    first commit

---
+++ Django and DRF (AI)/test.py
@@ -0,0 +1,12 @@
+#write python code for test
+
+def test():
+    print("Hello World")
+    print("This is a test")
+    print("This is a test")
+    print("This is a test")
+
+
+if __name__ == "__main__":
+    test()
+    print("This is a test")
