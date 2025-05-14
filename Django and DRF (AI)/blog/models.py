6a9fbd7dd05ec1ee61ea8d9a9d93389627ee59eb
mustaphahaadi <mustaphahaadi04@gmail.com>
2025-05-14 21:32:44 +0000

    chore: add BlogPost and Author models

--- Django and DRF (AI)/blog/models.py
+++ Django and DRF (AI)/blog/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Author(models.model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.name
+
+class BlogPost(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    publication_date = models.DateField()
+    is_published = models.BooleanField(default=False)
+    views = models.IntegerField(default=0)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
--- Django and DRF (AI)/blog/views.py
+++ Django and DRF (AI)/blog/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from djangorestframwork import viewsets
+from .models import Post
 
 # Create your views here.
+class PostViewset(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
